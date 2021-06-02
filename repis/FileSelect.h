34a416f5febd6b6069f8d49f4afc8c68e9de8dfe
BETTEY-developers <1175971310@qq.com>
2021-06-02 21:21:01 +0800

    添加项目文件。

---
+++ repis/FileSelect.h
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace repis
+{
+#define FILE_OPEN 0x10000
+#define FILE_CREATE 0XF1000
+#define FILE_CREATE_OR_OPEN 0X101000
+#define FILE_APPEND 0xF2000
+#define FILE_CLOSE 0xfffff
+
+	class FileInfo
+	{
+	public:
+		FileInfo()
+		{
+		}
+
+		~FileInfo()
+		{
+		}
+	protected:
+
+	private:
+		int* 
+	};
+
+}
